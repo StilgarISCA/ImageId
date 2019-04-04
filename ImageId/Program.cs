2ce5dfddd5972165b2eaac31351132f744626d1a
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2019-04-04 19:37:45 -0400

    Initial commit

---
+++ ImageId/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageId
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
